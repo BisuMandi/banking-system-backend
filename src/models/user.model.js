a6b624b4f4f46d55819a9621d66e985ed6a0c51f
BisuMandi <mandib694@gmail.com>
2026-09-13 19:19:49 +0530

    feat: define database schema for user

---
+++ src/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        trim: true,
+        lowercase: true,
+        match: [
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid email address"
+        ],
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password should contain atleast 6 characters"],
+        select: false
+    }
+}, { timestamps: true });
+
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+
+    next();
+});
+
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+export const UserModel = mongoose.model("User", userSchema);
